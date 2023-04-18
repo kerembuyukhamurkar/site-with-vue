a44c5cb78595b30a7d567500ef420ad57c11f230
kerem <kerembuyukhamurkar@gmail.com>
2023-04-18 19:41:24 +0300

    feat-created commentCard and commentPArt

---
+++ src/components/homepage/commentPart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-yellow-400 h-screen w-screen">
+        <commentCard/>
+    </div>
+    
+</template>
+
+<script>
+import commentCard from "./commentCard"
+export default{
+    components:{
+        commentCard
+
+    }
+}
+
+
+
+</script>
